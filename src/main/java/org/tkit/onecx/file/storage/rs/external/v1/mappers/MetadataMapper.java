e12ac261bdf3f87c9dcb34e98e0bd1219194149d
dpylak <203235923+dawpyl@users.noreply.github.com>
2026-03-13 13:58:10 +0100

    feat: remove redundant metadata response files

--- src/main/java/org/tkit/onecx/file/storage/rs/external/v1/mappers/MetadataMapper.java
+++ src/main/java/org/tkit/onecx/file/storage/rs/external/v1/mappers/MetadataMapper.java
@@ -1,9 +1,7 @@
 package org.tkit.onecx.file.storage.rs.external.v1.mappers;
 
-import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.MappingTarget;
 
 import gen.org.tkit.onecx.file.storage.rs.external.v1.model.FileMetadataResponseDTOV1;
 import software.amazon.awssdk.services.s3.model.HeadObjectResponse;
@@ -11,16 +9,7 @@ import software.amazon.awssdk.services.s3.model.HeadObjectResponse;
 @Mapper()
 public interface MetadataMapper {
 
-    @Mapping(target = "sizeUnit", ignore = true)
-    @Mapping(target = "storage", ignore = true)
     @Mapping(target = "size", expression = "java(head.contentLength())")
     @Mapping(target = "type", expression = "java(head.contentType())")
     FileMetadataResponseDTOV1 map(HeadObjectResponse head, String fileName);
-
-    @AfterMapping
-    default void setDefaultValues(HeadObjectResponse source, String fileName,
-            @MappingTarget FileMetadataResponseDTOV1 target) {
-        target.setSizeUnit("BYTES");
-        target.setStorage("rustfs");
-    }
 }
