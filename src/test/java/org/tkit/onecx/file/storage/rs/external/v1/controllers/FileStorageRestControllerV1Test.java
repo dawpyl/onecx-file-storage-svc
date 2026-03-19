7682c948201c6c26c4be6fc48f398cd07ab6a76e
Maciej Kryger <50527026+maciejkryger@users.noreply.github.com>
2026-03-19 09:47:50 +0100

    feat: 100% test coverage (#14)
    * feat: 100% Test Coverage - AbstractTest & FileStorageRestControllerV1Test
    
    * feat: 100% Test Coverage - S3APIServiceTest part1
    
    * feat: 100% Test Coverage - S3APIServiceTest part2
    
    * feat: 100% Test Coverage - AWS Dev services usage
    
    * feat: 100% Test Coverage - AWS Dev services usage - new way of tests
    
    * feat: 100% Test Coverage - AWS Dev services usage - new way of tests - removed unused old class
    
    * feat: 100% Test Coverage - AWS Dev services usage - new way of tests - sonar code smells remarks fixed

--- src/test/java/org/tkit/onecx/file/storage/rs/external/v1/controllers/FileStorageRestControllerV1Test.java
+++ src/test/java/org/tkit/onecx/file/storage/rs/external/v1/controllers/FileStorageRestControllerV1Test.java
@@ -1,6 +1,180 @@
 package org.tkit.onecx.file.storage.rs.external.v1.controllers;
 
-import org.tkit.onecx.file.storage.AbstractTest;
+import static io.restassured.RestAssured.given;
+import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
 
+import java.nio.charset.StandardCharsets;
+
+import jakarta.ws.rs.core.MediaType;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.tkit.onecx.file.storage.AbstractTest;
+import org.tkit.quarkus.security.test.GenerateKeycloakClient;
+
+import gen.org.tkit.onecx.file.storage.rs.external.v1.model.FileDownloadRequestDTOV1;
+import gen.org.tkit.onecx.file.storage.rs.external.v1.model.PresignedUrlRequestDTOV1;
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+@GenerateKeycloakClient(clientName = "testClient", scopes = { "ocx-fs:read", "ocx-fs:write", "ocx-fs:delete" })
 class FileStorageRestControllerV1Test extends AbstractTest {
+
+    String token;
+    String idToken;
+
+    @BeforeEach
+    void setup() {
+        token = keycloakClient.getClientAccessToken("testClient");
+        idToken = createToken("org1");
+    }
+
+    @Test
+    void uploadAndDownloadFileTest() {
+        byte[] fileContent = "onecx file content".getBytes(StandardCharsets.UTF_8);
+
+        given()
+                .auth().oauth2(token)
+                .header(APM_HEADER_PARAM, idToken)
+                .multiPart("applicationId", "app1")
+                .multiPart("productName", "product1")
+                .multiPart("fileName", "my-file.txt")
+                .multiPart("file", "my-file.txt", fileContent, "application/octet-stream")
+                .when()
+                .post("/v1/file-storage/file/upload")
+                .then()
+                .statusCode(201);
+
+        FileDownloadRequestDTOV1 request = new FileDownloadRequestDTOV1();
+        request.setFileName("my-file.txt");
+        request.setProductName("product1");
+        request.setApplicationId("app1");
+
+        given()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(request)
+                .when()
+                .auth().oauth2(token)
+                .header(APM_HEADER_PARAM, idToken)
+                .contentType(APPLICATION_JSON)
+                .post("/v1/file-storage/file/download")
+                .then()
+                .statusCode(200)
+                .contentType("application/octet-stream")
+                .extract()
+                .asByteArray();
+    }
+
+    @Test
+    void uploadFileBadRequestTest() {
+
+        given()
+                .auth().oauth2(token)
+                .header(APM_HEADER_PARAM, idToken)
+                .multiPart("applicationId", "app1")
+                .multiPart("productName", "product1")
+                .multiPart("fileName", "my-file.txt")
+                .when()
+                .post("/v1/file-storage/file/upload")
+                .then()
+                .statusCode(400);
+    }
+
+    @Test
+    void downloadFileNotFoundTest() {
+
+        FileDownloadRequestDTOV1 request = new FileDownloadRequestDTOV1();
+        request.setFileName("test-file.txt");
+        request.setProductName("product1");
+        request.setApplicationId("app1");
+
+        given()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(request)
+                .when()
+                .auth().oauth2(token)
+                .header(APM_HEADER_PARAM, idToken)
+                .contentType(APPLICATION_JSON)
+                .post("/v1/file-storage/file/download")
+                .then()
+                .statusCode(404);
+    }
+
+    @Test
+    void downloadFileGeneralExceptionTest() {
+
+        FileDownloadRequestDTOV1 request = new FileDownloadRequestDTOV1();
+        request.setProductName("product1");
+        request.setApplicationId("app1");
+
+        given()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(request)
+                .when()
+                .auth().oauth2(token)
+                .header(APM_HEADER_PARAM, idToken)
+                .contentType(APPLICATION_JSON)
+                .post("/v1/file-storage/file/download")
+                .then()
+                .statusCode(400);
+    }
+
+    @Test
+    void deleteFileTest() {
+        FileDownloadRequestDTOV1 request = new FileDownloadRequestDTOV1();
+        request.setFileName("test-file.txt");
+        request.setProductName("product1");
+        request.setApplicationId("app1");
+
+        given().contentType(MediaType.APPLICATION_JSON)
+                .body(request)
+                .when()
+                .auth().oauth2(token)
+                .header(APM_HEADER_PARAM, idToken)
+                .contentType(APPLICATION_JSON)
+                .post("/v1/file-storage/file/delete")
+                .then()
+                .statusCode(200);
+
+    }
+
+    @Test
+    void getPresignedUploadUrlTest() {
+
+        PresignedUrlRequestDTOV1 request = new PresignedUrlRequestDTOV1();
+        request.setFileName("test-file.txt");
+        request.setProductName("product1");
+        request.setApplicationId("app1");
+
+        given()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(request)
+                .when()
+                .auth().oauth2(token)
+                .header(APM_HEADER_PARAM, idToken)
+                .contentType(APPLICATION_JSON)
+                .post("/v1/file-storage/presigned/upload")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    void getPresignedDownloadUrlTest() {
+        PresignedUrlRequestDTOV1 request = new PresignedUrlRequestDTOV1();
+        request.setFileName("test-file.txt");
+        request.setProductName("product1");
+        request.setApplicationId("app1");
+
+        given()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(request)
+                .when()
+                .auth().oauth2(token)
+                .header(APM_HEADER_PARAM, idToken)
+                .contentType(APPLICATION_JSON)
+                .post("/v1/file-storage/presigned/download")
+                .then()
+                .statusCode(200);
+    }
+
 }
